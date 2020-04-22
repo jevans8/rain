4be4498be27bdb5c33f48a5de6629d2fa5a28887
Julia Evans <jevans8@mail.greenriver.edu>
2020-04-22 17:13:52 -0400

    Index page added

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the F3 Base class
+$f3 = Base::instance();
+
+// :: invokes static method
+// -> invokes instance method
+
+//Default route
+$f3->route('GET /', function(){
+    echo '<h1>Rain rain go away</h1>';
+});
+
+//Run F3
+$f3->run();
